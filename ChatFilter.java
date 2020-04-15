a122b8b91de2aaa4b41b0e496441f52542bad140
shenwei0102 <52152314+shenwei0102@users.noreply.github.com>
2020-04-15 13:45:33 -0400

    Add files via upload

---
+++ ChatFilter.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * [Add your documentation here]
+ *
+ * @author your name and section
+ * @version date
+ */
+public class ChatFilter {
+
+    public ChatFilter(String badWordsFileName) {
+
+    }
+
+    public String filter(String msg) {
+        return msg;
+    }
+}
---
+++ ChatMessage.java
@@ -0,0 +1,17 @@
+
+
+import java.io.Serializable;
+
+/**
+ *
+ * [Add your documentation here]
+ *
+ * @author your name and section
+ * @version date
+ */
+final class ChatMessage implements Serializable {
+    private static final long serialVersionUID = 6898543889087L;
+
+    // Here is where you should implement the chat message object.
+    // Variables, Constructors, Methods, etc.
+}
